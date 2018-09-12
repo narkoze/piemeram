bf38197157926bedf2d821e615c0e17cd45aa987
Edgars Vanags <ev@piemeram.lv>
2018-09-12 16:15:24 +0300

    Fixed table view on mobile

--- resources/assets/js/components/blog/admin/views/piemeram-blog-admin-view-posts.vue
+++ resources/assets/js/components/blog/admin/views/piemeram-blog-admin-view-posts.vue
@@ -2,94 +2,96 @@
   <div>
     <h1 class="title">{{ $t('blog.admin.views.blog-admin-view-posts.title') }}</h1>
 
-    <table class="table is-striped is-narrow is-hoverable is-fullwidth">
-      <thead>
-        <tr>
-          <th>{{ $t('blog.admin.views.blog-admin-view-posts.posttitle') }}</th>
-          <th>{{ $t('blog.admin.views.blog-admin-view-posts.categories') }}</th>
-          <th>{{ $t('blog.admin.views.blog-admin-view-posts.author') }}</th>
-          <th>{{ $t('blog.admin.views.blog-admin-view-posts.date') }}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-if="disabled">
-          <td
-            class="has-text-centered is-size-4"
-            colspan="3"
+    <div class="scrollable">
+      <table class="table is-striped is-narrow is-hoverable is-fullwidth">
+        <thead>
+          <tr>
+            <th>{{ $t('blog.admin.views.blog-admin-view-posts.posttitle') }}</th>
+            <th>{{ $t('blog.admin.views.blog-admin-view-posts.categories') }}</th>
+            <th>{{ $t('blog.admin.views.blog-admin-view-posts.author') }}</th>
+            <th>{{ $t('blog.admin.views.blog-admin-view-posts.date') }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-if="disabled">
+            <td
+              class="has-text-centered is-size-4"
+              colspan="3"
+            >
+              <i class="fas fa-spinner fa-pulse"></i>
+            </td>
+          </tr>
+          <tr
+            v-for="post in posts"
+            :key="post.id"
+            @mouseover="mouseover = post.id"
+            @mouseout="mouseover = null"
           >
-            <i class="fas fa-spinner fa-pulse"></i>
-          </td>
-        </tr>
-        <tr
-          v-for="post in posts"
-          :key="post.id"
-          @mouseover="mouseover = post.id"
-          @mouseout="mouseover = null"
-        >
-          <td>
-            <a @click="() => {
-              $root.activeSection = 'admin-view-posts'
-              $root.showView = 'admin-view-post'
-              $root.post = post
-            }">
-              <b>{{ post.title }}</b>
-            </a>
-
-            <div v-if="mouseover === post.id">
+            <td>
               <a @click="() => {
                 $root.activeSection = 'admin-view-posts'
                 $root.showView = 'admin-view-post'
                 $root.post = post
               }">
-                <small>{{ $t('blog.admin.views.blog-admin-view-posts.edit') }}</small>
+                <b>{{ post.title }}</b>
               </a>
-              <span class="link-divider">|</span>
-              <a @click="() => {
-                $root.showView = 'public-view-post'
-                $root.post = post
-              }">
-                <small v-if="post.published_at">{{ $t('blog.admin.views.blog-admin-view-posts.view') }}</small>
-                <small v-else>{{ $t('blog.admin.views.blog-admin-view-posts.preview') }}</small>
-              </a>
-            </div>
-          </td>
 
-          <td>
-            <span
-              v-for="(category, index) in post.categories"
-              :key="category.id"
-            >
-              {{ category.name }}<span v-if="post.categories.length - index !== 1">,</span>
-            </span>
-          </td>
+              <div v-if="mouseover === post.id">
+                <a @click="() => {
+                  $root.activeSection = 'admin-view-posts'
+                  $root.showView = 'admin-view-post'
+                  $root.post = post
+                }">
+                  <small>{{ $t('blog.admin.views.blog-admin-view-posts.edit') }}</small>
+                </a>
+                <span class="link-divider">|</span>
+                <a @click="() => {
+                  $root.showView = 'public-view-post'
+                  $root.post = post
+                }">
+                  <small v-if="post.published_at">{{ $t('blog.admin.views.blog-admin-view-posts.view') }}</small>
+                  <small v-else>{{ $t('blog.admin.views.blog-admin-view-posts.preview') }}</small>
+                </a>
+              </div>
+            </td>
 
-          <td>{{ post.author.name }}</td>
-
-          <td>
-            <div v-if="post.published_at">
-              {{ $t('blog.admin.views.blog-admin-view-posts.published') }}
-              <br>
+            <td>
               <span
-                class="hover"
-                :title="post.published_at"
+                v-for="(category, index) in post.categories"
+                :key="category.id"
               >
-                {{ post.published_at | dateString }}
+                {{ category.name }}<span v-if="post.categories.length - index !== 1">,</span>
               </span>
-            </div>
-            <div v-else>
-              {{ $t('blog.admin.views.blog-admin-view-posts.saved') }}
-              <br>
-              <span
-                class="hover"
-                :title="post.updated_at"
-              >
-                {{ post.updated_at | dateString }}
-              </span>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+            </td>
+
+            <td>{{ post.author.name }}</td>
+
+            <td>
+              <div v-if="post.published_at">
+                {{ $t('blog.admin.views.blog-admin-view-posts.published') }}
+                <br>
+                <span
+                  class="hover"
+                  :title="post.published_at"
+                >
+                  {{ post.published_at | dateString }}
+                </span>
+              </div>
+              <div v-else>
+                {{ $t('blog.admin.views.blog-admin-view-posts.saved') }}
+                <br>
+                <span
+                  class="hover"
+                  :title="post.updated_at"
+                >
+                  {{ post.updated_at | dateString }}
+                </span>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
