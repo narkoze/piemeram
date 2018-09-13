9eb2caa93d3baf90f1020267130ab3b74d62b595
Edgars Vanags <ev@piemeram.lv>
2018-09-13 12:55:03 +0300

    Improved prod script

--- bin/build.production.sh
+++ bin/build.production.sh
@@ -22,12 +22,35 @@ if [ -d public ]; then
   mv public public_html
   sed -i '/\workaround/s/^\/\///g' app/Providers/AppServiceProvider.php
 fi
-tar -zcvf piem.tgz .
-ls -l --block-size=M | grep piem.tgz
-scp piem.tgz piem:/home/piemeram/
-ssh piem touch /home/piemeram/storage/framework/down
-ssh piem tar -xvzf /home/piemeram/piem.tgz
-ssh piem rm /home/piemeram/storage/framework/down /home/piemeram/piem.tgz
+
+ssh piem 'sh /home/piemeram/cleanup.sh'
+tar czv public_html | ssh piem 'cat | tar xz -C /home/piemeram/'
+tar czv \
+  --exclude public_html \
+  --exclude database \
+  --exclude bin \
+  --exclude tests \
+  --exclude artisan \
+  --exclude .git \
+  --exclude .vscode \
+  --exclude .editorconfig \
+  --exclude .env.example \
+  --exclude .eslintrc.json \
+  --exclude .gitattributes \
+  --exclude .gitignore \
+  --exclude composer.json \
+  --exclude composer.lock \
+  --exclude yarn.lock \
+  --exclude package-lock.json \
+  --exclude package.json \
+  --exclude phpunit.xml \
+  --exclude README.md \
+  --exclude server.php \
+  --exclude webpack.mix.js \
+./ | ssh piem 'cat | tar xz -C /home/piemeram/laravel'
+ssh piem 'sh /home/piemeram/indexfix.sh'
+
+
 # php artisan migrate --pretend
 
 cd -
